ff7f72f70499941f600b720232bb83fe9d585093
ForgeGithub <jruizcabrejos@gmail.com>
2021-01-03 06:42:14 -0500

    Create import_db.R

---
+++ R/import_db.R
@@ -0,0 +1,30 @@
+#' Loads district data for Perú
+#'
+#'Function 'import_db' the specified dataset.
+#'
+#' @param dataset is a character specifiying the dataset to request.
+#'
+#' Current datasets available are:
+#'  - "Perú names": Returns the name of the 1874 districts, 196 provinces and 25 regions of Perú as they are reported by the INEI in the REDATAM platform for the 2017 CENSUS.
+#'
+#' @examples df <- data.frame(reg=c("LIMA","CALLAO","CAJAMARCA","AMAZONAS","SAN MARTIN","HUANUCO",
+#' "PASCO","JUNIN","CUSCO", "PUNO","APURIMAC","AYACUCHO",
+#' "HUANCAVELICA","TUMBES","PIURA","LAMBAYEQUE","LA LIBERTAD",
+#' "ANCASH","ICA","AREQUIPA","TACNA","MOQUEGUA","LORETO","UCAYALI",
+#' "MADRE DE DIOS"),
+#' stringsAsFactors = FALSE)
+#'
+#'
+#'
+#' @export import_db
+import_db <- function(dataset) {
+
+
+  if(dataset=="Perú names") {
+
+    x <- getURL("https://raw.githubusercontent.com/healthinnovation/lis/master/files/master_distr.csv")
+    x <- read.csv(text = x)
+  }
+
+  return(x)
+}
