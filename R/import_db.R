1d6bd423780e7ad56e91214e2859dc33dc9d9820
ForgeGithub <jruizcabrejos@gmail.com>
2021-01-03 20:23:59 -0500

    roxygenise

--- R/import_db.R
+++ R/import_db.R
@@ -5,8 +5,8 @@
 #' @param dataset is a character specifiying the dataset to request.
 #'
 #' Current datasets available are:
-#'  - "Perú names": Returns the name of the 1874 districts, 196 provinces and 25 regions of Perú as they are reported by the INEI in the REDATAM platform for the 2017 CENSUS.
-#'
+#' - "Perú names": Returns the name of the 1874 districts, 196 provinces and 25 regions of Perú as they are reported by the INEI in the REDATAM platform for the 2017 CENSUS.
+#' - "Perú shp": Returns the shapefile for thethe 1874 districts, 196 provinces and 25 regions of Perú
 #' @examples df <- data.frame(reg=c("LIMA","CALLAO","CAJAMARCA","AMAZONAS","SAN MARTIN","HUANUCO",
 #' "PASCO","JUNIN","CUSCO", "PUNO","APURIMAC","AYACUCHO",
 #' "HUANCAVELICA","TUMBES","PIURA","LAMBAYEQUE","LA LIBERTAD",
@@ -23,7 +23,13 @@ import_db <- function(dataset) {
   if(dataset=="Perú names") {
 
     x <- getURL("https://raw.githubusercontent.com/healthinnovation/lis/master/files/master_distr.csv")
-    x <- read.csv(text = x)
+    x <- read.csv(text = x, stringsAsFactors = F)
+
+  } else if(dataset=="Perú shp") {
+
+    x <- #"https://github.com/thefactmachine/hex-binning-gis-data/raw/master/popDensity.RData"
+    x <- readRDS(url(x))
+
   }
 
   return(x)
