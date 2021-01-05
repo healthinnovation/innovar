fe82cb4702bc7683cbfc4418c1e883e6b1506daf
ForgeGithub <jruizcabrejos@gmail.com>
2021-01-05 02:48:15 -0500

    Rename districts function (WIP)

--- R/hello.R
+++ R/hello.R
@@ -2,11 +2,13 @@
 #'
 #' Prints 'Hello, world!'.
 #'
+#' @param argument is a character specifiying the dataset to request.
 #'
 #' @examples
 #'
 #' hello()
 #'
+#' @import dplyr
 #' @export hello
 hello <- function() {
   print("Hello, world!")
