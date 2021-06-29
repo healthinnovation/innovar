b80870c4c56d20c4ce3e137ab97a53fa829741b3
Brian Norman Peña Calero <brianmsm@gmail.com>
2021-06-29 18:53:30 -0500

    Reordenamiento de función

--- R/allpautils_names.R
+++ R/allpautils_names.R
@@ -2,16 +2,22 @@
 #'
 #' Function 'allpautils_names' revisar: https://cran.r-project.org/web/packages/MazamaSpatialUtils/vignettes/MazamaSpatialUtils.html
 #'
-#' @param x vector of characters
+#' @param string vector of characters
 #'
 #' @examples df
 #'
 #' @export allpautils_names
-allpautils_names <- function(x) {
+allpautils_names <- function(string) {
 
-  x <- gsub("_", " ",gsub("-", " ", iconv(x,
-                                         from="UTF-8",
-                                         to="ASCII//TRANSLIT"), fixed=TRUE), fixed=TRUE)
+  string <- gsub(pattern = "_",
+                 replacement = " ",
+                 x = gsub(pattern = "-",
+                          replacement = " ",
+                          x = iconv(string,
+                                    from="UTF-8",
+                                    to="ASCII//TRANSLIT"),
+                          fixed=TRUE),
+                 fixed=TRUE)
 
-  return(x)
+  return(string)
 }
