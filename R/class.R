b64f205789183e88012e9318bde36eb3030bfed6
Brian Norman Peña Calero <brianmsm@gmail.com>
2021-11-05 19:58:53 -0500

    Refactoring, add example and initial support for class outputs

--- R/class.R
+++ R/class.R
@@ -13,11 +13,26 @@ setClass("indexFit",
          )
 )
 
+setClass("indexFits",
+         slots = c(
+           Specific = "list",
+           Fits = "list",
+           Explained = "tbl_df",
+           Loadings = "tbl_df"
+         ),
+         prototype = list(
+           Specific = list(),
+           Fits = list(),
+           Explained = NA_real_,
+           Loadings = NA_real_
+         )
+)
+
 
 setClass("indexcalc",
          slots = c(
            Options = "list",
            Data = "tbl_df",
-           Fit = "indexFit"
+           Fit = "indexFits"
          )
 )
